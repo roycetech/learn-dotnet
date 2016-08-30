3657d6171706e29a48b53ff6d77b7f5d6241925c
Royce Remulla <royce.com@gmail.com>
2016-08-30 13:12:24 +0800

    Completed Code School Level 1
    Removed unused 'using' directives
    Cleaned up template layout

--- learn-asp/CharacterSheetAppGrunt/Controllers/HomeController.cs
+++ learn-asp/CharacterSheetAppGrunt/Controllers/HomeController.cs
@@ -1,35 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+
 
 namespace CharacterSheetAppGrunt.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    public IActionResult Index()
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View();
-        }
+      var name = "Sword";
+      
+      return View("Index", name);
     }
+  }
 }
---
+++ learn-asp/CharacterSheetAppGrunt/Models/Equipment.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace CharacterSheetApp.Models
+{
+  public class Equipment
+  {
+    public String Name;
+  }
+}
+
