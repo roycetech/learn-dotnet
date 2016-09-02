a3a0c29dce7b49707f356c4546c9e0a109686039
Royce Remulla <royce.com@gmail.com>
2016-09-02 12:15:29 +0800

    Completed 3.6

--- learn-asp/ForgingAhead/Controllers/HomeController.cs
+++ learn-asp/ForgingAhead/Controllers/HomeController.cs
@@ -1,30 +1,21 @@
-// using System;
-// using System.Collections.Generic;
-// using System.Linq;
-// using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using ForgingAhead.Models;
 
 namespace ForgingAhead.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context) {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
+            var model = _context.Characters.Where(e => e.IsActive).ToList();
+            return View(model);
         }
 
         public IActionResult Error()
