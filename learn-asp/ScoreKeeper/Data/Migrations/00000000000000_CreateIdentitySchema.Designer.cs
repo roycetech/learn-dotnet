a94654e915c18bcfadd6d6f9194765f098d4c32e
Royce Remulla <royce.com@gmail.com>
2016-09-03 14:38:21 +0800

    Removed unused using directives

--- learn-asp/ScoreKeeper/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
+++ learn-asp/ScoreKeeper/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
@@ -3,7 +3,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
-using WebApplication.Data;
 
 namespace WebApplication.Data.Migrations
 {
